022b5c2eaa1f3342479d3dfd74003eac168c4966
Maksym <bebeshkomaksym1997@gmail.com>
2023-12-11 11:34:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// let number = 12;
+//  if (number % 3 == 0 && number % 5 == 0){
+//     console.log('FizzBuzz');
+//  } else if (number % 5 == 0){
+//     console.log('Buzz');
+//  } else if (number % 3 == 0){
+//     console.log('Fizz');
+//  } 
+ 
+// let year = 2023;
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//     console.log(year + " є високосним роком.");
+// } else {
+//     console.log(year + " не є високосним роком.");
+// }
+
+// let years = 101;
+
+// if ((years % 10 === 1 && years % 100 !== 11)){
+//     console.log('Вам ' + years + ' рік')
+// }else if ((years % 10 >= 2 && years % 10 <= 4) && (years % 100 < 10 || years % 100 >= 20)){
+//     console.log('Вам ' + years + ' роки')
+// }else {
+//     console.log('Вам ' + years + ' років')
+// }
